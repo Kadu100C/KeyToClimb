86dcea4eab337d82627362454dabede36f53d1f3
Kadu100C <kadujgkdc@gmail.com>
2025-06-19 10:49:57 -0300

    KeyToClimb

---
+++ key-to-climb/Scenes/telainicial.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/ficha_do_inimigo.tscn")
+
+
+
+
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
