79952c6a6decb9e733892472608040f3293fc13a
Kadu100C <kadujgkdc@gmail.com>
2025-06-19 17:28:36 -0300

    KeyToClimb
    adicionado: Menu, Créditos, Pause e resolução 4:3

---
+++ key-to-climb/Scenes/pause_menu.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+@onready var resume: Button = $VBoxContainer/Resume
+@onready var main_menu: Button = $"VBoxContainer/Main Menu"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	visible = false
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		visible = true
+		get_tree().paused = true
+
+
+func _on_resume_pressed() -> void:
+	get_tree().paused = false
+	visible = false
+
+
+func _on_main_menu_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://Scenes/telainicial.tscn")
--- key-to-climb/Scripts/telainicial.gd
+++ key-to-climb/Scripts/telainicial.gd
@@ -6,9 +6,9 @@ func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/ficha_do_inimigo.tscn")
 
 
-
-
-
-
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
